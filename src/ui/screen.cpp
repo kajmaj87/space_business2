9e4fac7f7c2c3ad0a9fdd5d33847ba8e6a24b20d
kajman <kajmaj87@gmail.com>
2022-05-06 21:13:47 +0200

    Printing mouse position

--- src/ui/screen.cpp
+++ src/ui/screen.cpp
@@ -12,6 +12,9 @@ const auto TOP = 2, LEFT = 0, RIGHT = 20, BOTTOM = 5;
 
 void UIScreen::initialize(Engine &engine) {
   using namespace components;
+  int mouse_x = 0;
+  int mouse_y = 0;
+
   auto middle = Renderer([&] {
     auto c = ftxui::Canvas(CANVAS_WITDH, CANVAS_HEIGHT);
     auto view = engine.registry->view<Position>();
@@ -27,7 +30,7 @@ void UIScreen::initialize(Engine &engine) {
                 text("+ / - Change ticks per second (TPS)"),
                 text("[ / ] Change seconds per tick (SPT)")) | center;
   });
-  auto right = Renderer([] { return text("right") | center; });
+  auto right = Renderer([&] { return text(fmt::format("({},{}): {}", mouse_x, mouse_y, "nothing")) | center; });
   auto top = Renderer([&] {
     auto &time = engine.registry->ctx<Time>();
     auto &timeStats = engine.registry->ctx<TimeStats>();
@@ -35,7 +38,7 @@ void UIScreen::initialize(Engine &engine) {
     auto rest = timeStats.simulationSpeed % 86400;
     return vbox(text(fmt::format("{}.{:02}.{:02} {:02}:{:02}", time.year, time.month,
                             time.day, time.hour, time.minutes)) | align_right,
-                text(fmt::format("Game time advances {}days and {:%Hh%Mm%Ss} every second", days, std::chrono::seconds{rest})),
+                text(fmt::format("Game time advances {} days and {:%Hh%Mm%Ss} every second", days, std::chrono::seconds{rest})),
                 text(fmt::format("Efficiency: {:.3}%", 100*timeStats.simulationEfficiency(time))),
                 text(fmt::format("Requested TPS: {}", time.updatesPerSecond)),
                 text(fmt::format("Requested SPT: {}", time.secondsLastTick())),
@@ -63,6 +66,10 @@ void UIScreen::initialize(Engine &engine) {
   renderer = CatchEvent(renderer, [&](Event event) {
     if (event.is_character()) {
       engine.dispatcher->enqueue<events::key_pressed>(event.character());
+    }
+    if (event.is_mouse()) {
+      mouse_x = (event.mouse().x - 1) * 2;
+      mouse_y = (event.mouse().y - 1) * 4;
     }
     return false;
   });
