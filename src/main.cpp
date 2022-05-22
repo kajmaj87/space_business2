6b531317a03066333a8b87874c6fdba420254414
kajman <kajmaj87@gmail.com>
2022-05-22 10:26:18 +0200

    Fixed ctrl+c bug

--- src/main.cpp
+++ src/main.cpp
@@ -3,43 +3,27 @@
 #include <csignal>
 #include <chrono>
 
-std::atomic<bool> running = true;
-std::function<void()> shutdown_handler;
-void signal_handler(int signal) {
-    shutdown_handler();
-}
+std::atomic_bool running = true;
 
 auto main() -> int {
   UIScreen uiScreen;
 
-    signal(SIGINT, signal_handler);
   Engine engine = Engine();
-  std::thread game_loop([&engine, &uiScreen]() { 
-    // signal(SIGINT, signal_handler);
+  std::thread game_loop([&engine]() { 
     engine.loop(running); 
-    printf("Engine loop exited");
-    // uiScreen.exit();
   });
-  std::thread render_loop([&engine, &uiScreen]() { 
-    // signal(SIGINT, signal_handler);
+  std::thread render_loop([&uiScreen]() { 
     while(running){
       using namespace std::chrono_literals;
       uiScreen.render(); 
       std::this_thread::sleep_for(60ms);
     }
-    printf("Render loop exited");
   });
   
-  shutdown_handler = [&engine, &uiScreen](){
-    //  printf("Recieved interrupt signal");
-    //  engine.registry->ctx<GameState>().running = false; 
-     running = false;
-     uiScreen.exit();
-  };
   uiScreen.initialize(engine);
+  running = false;
   game_loop.join();
   render_loop.join();
-  printf("Main exited");
 
   return 0;
 }
