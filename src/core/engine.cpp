9cc77d7e49923bb6fc14dbe2a05136d8e139a6c3
Kajetan Maj <kajetan.maj@capgemini.com>
2022-05-27 10:15:09 +0200

    Added other planets

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -26,26 +26,50 @@ void Engine::prepareScene() {
   registry->emplace<Position>(sun, 0.0, 0.0);
   registry->emplace<Mass>(sun, 1.9885e30);
   const auto mercury = registry->create();
+  const auto venus = registry->create();
   const auto moon = registry->create();
   const auto earth = registry->create();
+  const auto mars = registry->create();
   const auto jupiter = registry->create();
   const auto saturn = registry->create();
+  const auto uran = registry->create();
+  const auto neptun = registry->create();
   using namespace constants::physics;
   registry->emplace<Position>(mercury, 0.0, 0.387098_au);
   registry->emplace<Mass>(mercury, 3.3011e23);
   registry->emplace<Orbit>(mercury, mercury, sun, registry);
+
+  registry->emplace<Position>(venus, 0.0, 0.723332_au);
+  registry->emplace<Mass>(venus, 4.8675e24);
+  registry->emplace<Orbit>(venus, venus, sun, registry);
+
   registry->emplace<Position>(earth, 0.0, 1.0_au);
   registry->emplace<Mass>(earth, 5.97237e24);
   registry->emplace<Orbit>(earth, earth, sun, registry);
+
   registry->emplace<Position>(moon, 0.0, 1.00257_au);
   registry->emplace<Mass>(moon, 7.342e22);
   registry->emplace<Orbit>(moon, moon, earth, registry);
+
+  registry->emplace<Position>(mars, 0.0, 1.52368055_au);
+  registry->emplace<Mass>(mars, 6.4171e23);
+  registry->emplace<Orbit>(mars, mars, sun, registry);
+
   registry->emplace<Position>(jupiter, 0.0, 5.2038_au);
   registry->emplace<Mass>(jupiter, 1.8982e27);
   registry->emplace<Orbit>(jupiter, jupiter, sun, registry);
+
   registry->emplace<Position>(saturn, 0.0, 9.5826_au);
   registry->emplace<Mass>(saturn, 5.6834e26);
   registry->emplace<Orbit>(saturn, saturn, sun, registry);
+
+  registry->emplace<Position>(uran, 0.0, 19.19126_au);
+  registry->emplace<Mass>(uran, 8.681e25);
+  registry->emplace<Orbit>(uran, uran, sun, registry);
+
+  registry->emplace<Position>(neptun, 0.0, 30.07_au);
+  registry->emplace<Mass>(neptun, 1.02413e26);
+  registry->emplace<Orbit>(neptun, neptun, sun, registry);
 }
 
 void Engine::loop() {
